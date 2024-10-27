b033b72c6f6b7e1aa6ed073c2f2981bd4fe099ed
Panagiotis Chatzigiannis <pchatzijohn@gmail.com>
2024-10-27 12:51:50 +0200

    Fix: changed navbar

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-
+  const menu = ["About", "Skills", "Projects", "Contact"]
 </script>
 
 <template>
@@ -7,13 +7,12 @@
     <div class="flex flex-row w-full min-h-full items-center">
       <h1 class="text-lg font-bold text-slate-900">Panagiotis Chatzigiannis</h1>
     </div>
-    <div class="flex items-center justify-center min-h-full w-full">
-      <menu class="justify-between gap-4 items-center hidden md:inline-flex text-lg">
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </menu>
+    <div class="flex flex-row items-center justify-center min-h-full w-full">
+      <nav>
+        <ul class="justify-between gap-4 items-center hidden md:inline-flex text-lg">
+          <li v-for="item in menu" v-bind:key="item"><button class="bg-transparent hover:text-white hover:bg-black transition-all duration-200 rounded-md p-2" >{{item}}</button></li>
+        </ul>
+      </nav>
     </div>
     <div class="flex gap-1 w-full items-center justify-end">
       <button class="bg-transparent hover:text-white hover:bg-black transition-all duration-200 rounded-md h-12 w-12">
