cc1f0aa8ce0e54c3b215b0ec48451edf430c566c
chatzijohn <axpanos@yahoo.gr>
2024-10-28 18:02:22 +0200

    Feat: added routing

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -1,16 +1,32 @@
 <script lang="ts" setup>
-  const menu = ["About", "Skills", "Projects", "Contact"]
+const menu = [
+  { name: "About", path: "/about" },
+  { name: "Skills", path: "/skills" },
+  { name: "Projects", path: "/projects" },
+  { name: "Contact", path: "/contact" }
+];
 </script>
 
 <template>
   <div class="flex flex-row items-center max-w-dvw p-2 h-16 ">
     <div class="flex flex-row w-full min-h-full items-center">
-      <h1 class="text-lg font-bold text-slate-900">Panagiotis Chatzigiannis</h1>
+      <NuxtLink to="/">
+        <h1 class="text-lg font-bold text-slate-900">
+          Panagiotis Chatzigiannis
+        </h1>
+      </NuxtLink>
     </div>
     <div class="flex flex-row items-center justify-center min-h-full w-full">
       <nav>
         <ul class="justify-between gap-4 items-center hidden md:inline-flex text-lg">
-          <li v-for="item in menu" v-bind:key="item"><button class="bg-transparent hover:text-white hover:bg-black transition-all duration-200 rounded-md p-2" >{{item}}</button></li>
+          <li v-for="item in menu" v-bind:key="item.name">
+            <NuxtLink :to="item.path">
+              <button
+                class="bg-transparent hover:text-white hover:bg-black transition-all duration-200 rounded-md p-2">{{
+                  item.name
+                }}</button>
+            </NuxtLink>
+          </li>
         </ul>
       </nav>
     </div>
