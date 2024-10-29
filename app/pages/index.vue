44a231908318175435d3afa90edf54df96360b1a
Panagiotis Chatzigiannis <pchatzigiannis@technor.gr>
2024-10-29 09:48:48 +0200

    Feat: Dark mode switcher

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-    <div class="flex justify-center min-h-full bg-red-500">
-        <h1 class="text-lg text-blue-500">Home Page</h1>
+    <div class="flex justify-center min-h-full">
+
+        <h1>Color mode: {{ $colorMode.value }}</h1>
     </div>
 </template>
