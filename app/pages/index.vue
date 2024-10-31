de3f9fddf6c12cf77ff736e95e5fb710bb6b3d6b
Panagiotis Chatzigiannis <pchatzigiannis@technor.gr>
2024-10-31 09:39:12 +0200

    Fix: replaced img with NuxtImg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,12 @@
- 
 <template>
     <div class="flex flex-col sm:flex-row justify-center items-center">
         <div class="flex items-center w-32 h-32 m-4 rounded-full border-2 border-border border-md overflow-hidden">
-            <img src="https://avatars.githubusercontent.com/chatzijohn" class="w-32 h-32 object-cover"/>
+            <NuxtImg src="https://avatars.githubusercontent.com/chatzijohn" class="w-32 h-32 object-cover" />
         </div>
         <div class="items-center min-h-full w-48 sm:w-64">
             <h1 class="text-xl">About me</h1>
-            <p class="text-md">I'm a self-taught Full Stack Web Developer with an Electrical Engineering background. I try to build more, but so far, I mostly break...</p>
+            <p class="text-md">I'm a self-taught Full Stack Web Developer with an Electrical Engineering background. I
+                try to build more, but so far, I mostly break...</p>
         </div>
     </div>
 </template>
